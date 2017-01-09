e7480332ca8c9111ba8107663e6bc0712cc007a7
Eric <schaikejgv@gmail.com>
2017-01-09 14:13:43 +0100

    Startscreen finished

--- app/src/main/java/hci/cityplanner/StartActivity.java
+++ app/src/main/java/hci/cityplanner/StartActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -16,5 +17,10 @@ public class StartActivity extends AppCompatActivity {
     public void openMap(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+    }
+
+    public void buttonPress(View view) {
+        ImageButton imgbtn = (ImageButton)view;
+        imgbtn.setAlpha((float)(imgbtn.getAlpha()>0.7 ? 0.5 : 1));
     }
 }
